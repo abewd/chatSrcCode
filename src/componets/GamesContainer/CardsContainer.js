69a4049016eb54f7a7b76cd2a98f57159fc1b679
Abewd <116496332+abewd@users.noreply.github.com>
2023-04-25 00:40:02 +1000

    added better styling

--- src/componets/GamesContainer/CardsContainer.js
+++ src/componets/GamesContainer/CardsContainer.js
@@ -7,18 +7,19 @@ import {
   Avatar,
 } from "@material-tailwind/react";
 import { StarIcon } from "@heroicons/react/24/solid";
+
 const CardsContainer = () => {
   return (
     <Card
       color="transparent"
       shadow={false}
-      className="w-full max-w-[26rem] bg-blue-200"
+      className="w-full max-w-[26rem] bg-blue-200 rounded-lg overflow-hidden"
     >
       <CardHeader
         color="transparent"
         floated={false}
         shadow={false}
-        className="mx-0 flex items-center gap-4 pt-0 pb-8"
+        className="mx-0 flex items-center gap-4 pt-6 pb-4 px-6 bg-gradient-to-b from-blue-300 to-blue-500"
       >
         <Avatar
           style={{ width: "40px", height: "40px" }}
@@ -27,9 +28,9 @@ const CardsContainer = () => {
           src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
           alt="candice wu"
         />
-        <div className="flex w-full flex-col gap-0.5">
+        <div className="flex w-full flex-col gap-1">
           <div className="flex items-center justify-between">
-            <Typography variant="h5" color="blue-gray">
+            <Typography variant="h5" color="white">
               Candice Wu
             </Typography>
             <div className="5 flex items-center gap-0">
@@ -40,11 +41,13 @@ const CardsContainer = () => {
               <StarIcon className="h-5 w-5 text-yellow-700" />
             </div>
           </div>
-          <Typography color="blue-gray">Frontend Lead @ Google</Typography>
+          <Typography color="white" className="text-sm">
+            Frontend Lead @ Google
+          </Typography>
         </div>
       </CardHeader>
-      <CardBody className="mb-6 p-0">
-        <Typography>
+      <CardBody className="px-6 pt-4 pb-6">
+        <Typography className="text-gray-700 text-base leading-relaxed">
           &quot;I found solution to all my design needs from Creative Tim. I use
           them as a freelancer in my hobby projects for fun! And its really
           affordable, very humble guys !!!&quot;

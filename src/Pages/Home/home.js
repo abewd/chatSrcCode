de1c49dc4154de8433894454e4aeeb4536a1d7e9
Abewd <116496332+abewd@users.noreply.github.com>
2023-04-25 22:10:01 +1000

    need to edit the sizing for the home page too

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -6,20 +6,26 @@ import GamesContainer from "../../componets/GamesContainer/GamesContainer";
 
 const home = () => {
   return (
-    <div className="p-4" style={{ height: "100vh" }}>
-      <div className="flex flex-row animationContainer rounded-lg m-3">
-        <div class="basis-1/2 m-3 p-3 ">
-          {" "}
-          <GamesContainer />
-        </div>
-        <div className="basis-1/2 p-8">
-          <Animations />
-        </div>
-      </div>
+    <div class="bg-gray-50 min-h-screen flex items-center justify-center px-16">
+      <div class="relative w-full max-w-lg">
+        <div class="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+        <div class="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+        <div class="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+        <div class="m-8 relative space-y-4">
+          <div className="p-4" style={{ height: "100vh" }}>
+            <div className="flex flex-row animationContainer rounded-lg m-3">
+              <div class="basis-1/2 m-3 p-3 ">
+                {" "}
+                <GamesContainer />
+              </div>
+              <div className="basis-1/2 p-8">
+                <Animations />
+              </div>
+            </div>
 
-      {/* IF YOU WANT TO ADD MORE CARDS AND GIFS, USE THIS */}
+            {/* IF YOU WANT TO ADD MORE CARDS AND GIFS, USE THIS */}
 
-      {/* <div className="flex flex-row animationContainer rounded-lg m-3">
+            {/* <div className="flex flex-row animationContainer rounded-lg m-3">
         <div class="basis-1/2 m-3 p-3 ">
           {" "}
           <Animations />
@@ -28,6 +34,9 @@ const home = () => {
           <GamesContainer />
         </div>
       </div> */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

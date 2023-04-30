4f0de45e22c23696ea1b8e6fa3c62a3b4a98209b
Abewd <116496332+abewd@users.noreply.github.com>
2023-04-30 12:36:25 +1000

    removed unused files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import Register from "./componets/Register/index";
 // import Home from "./componets/Home/index";
 import Signin from "./componets/Signin/index";
-import Landing from "./componets/Home/index";
+// import Landing from "./componets/Home/index";
 import Home from "./Pages/Home/home";
 import Login from "./componets/Login/Login";
 import Header from "./componets/Header/index";
